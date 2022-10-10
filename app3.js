9949f3c4037b67872ce230bf03baef9d4add0b48
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-10-10 14:52:07 +0000

    [need more Learn] about argument

--- app3.js
+++ app3.js
@@ -1,7 +1,23 @@
 let user_hand = prompt('ジャンケンの手を、グー、チョキ、パーから選んでください。');
 
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
 alert('あなたの選んだ手は、' + user_hand + 'です。');
 
 function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
   
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  }else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  }else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+  
+  return js_hand;
 }
+
+function winLose()
